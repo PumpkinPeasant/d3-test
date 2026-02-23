b9dca924584d70b6c7b8f9848e51c91217eaac23
PumpkinPeasant <m2314670@edu.misis.ru>
2026-02-23 21:38:01 +0300

    Added bars hover

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import {axisBottom, axisLeft, max, min, scaleBand, scaleUtc, select} from "d3";
+import {axisBottom, axisLeft, color, index, interpolateLab, max, min, rgb, scaleBand, scaleUtc, select} from "d3";
 import {type Director, directorsMockData} from "../mocks/directors.mock";
+import {S} from "vue-router/dist/router-CWoNjPRp";
 
-const data = directorsMockData.sort((a:Director, b:Director) => new Date(a.from).getTime() - new Date(b.from).getTime())
+const data = directorsMockData.sort((a: Director, b: Director) => new Date(a.from).getTime() - new Date(b.from).getTime())
 
 const graphContainer = ref<HTMLElement | null>(null);
 
@@ -10,6 +11,9 @@ let svgContainer: any
 
 const width = 1000
 const height = 400
+
+const startColor = '#0061ff'
+const endColor = '#60efff'
 
 /* Рассчет отступов */
 const calculateMarginLeft = (): number => {
@@ -45,6 +49,8 @@ const renderGraph = () => {
       .attr('transform', `translate(${margin.left},0)`)
       .call(axisLeft(yScale))
 
+  const colorScale = interpolateLab(rgb(startColor), rgb(endColor));
+
   // Добавление данных
   svgContainer.selectAll('.bar')
       .data(data)
@@ -53,10 +59,24 @@ const renderGraph = () => {
       .attr('y', (d: Director) => yScale(d.name))
       .attr("height", yScale.bandwidth())
       .attr("width", (d: Director) => xScale(!!d.to ? new Date(d.to) : new Date()) - xScale(new Date(d.from)))
-      .attr("fill", '#be97fc')
+      .attr("fill", (d: Director, i: number, nodes: Director[]) => {
+        return colorScale(nodes.length / 100 * i);
+      })
+      .attr("data-color", (d: Director, i: number, nodes: Director[]) => {
+        return colorScale(nodes.length / 100 * i);
+      })
+      .style('cursor', 'pointer')
+      .on('mouseenter', function (this: SVGRectElement, d: Director, i: number, nodes: Director[]) {
+        const rect = select(this)
+        const currentColor = rect.attr('data-color');
+        rect.attr("fill", String(rgb(currentColor).brighter(0.2)))
+      })
+      .on('mouseleave', function (this: SVGRectElement) {
+        const rect = select(this)
+        const currentColor = rect.attr('data-color');
+        rect.attr("fill", String(rgb(currentColor)))
+      })
 }
-
-console.log()
 
 
 onMounted(() => {
